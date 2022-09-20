4cf9d8caf6edd515176a2b30d5d5fcaee88ef27f
J.K <jnuprasad@gmail.com>
2022-09-20 23:23:01 +0530

    Compose way Lazy Colum ~ ScrollView

---
+++ app/src/main/java/com/github/paviour/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.github.paviour
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.paviour.ui.theme.PaviourTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PaviourTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    App()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    LazyColumn {
+        item {
+            Column(
+                modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxWidth()
+                    .height(720.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    TopSection(id = R.drawable.img)
+                }
+            }
+        }
+
+        item {
+            Column(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxWidth()
+                    .height(500.dp)
+            ) {
+                MiddleSection()
+            }
+        }
+
+        item {
+            Column(
+                modifier = Modifier
+                    .background(Color.Yellow)
+                    .fillMaxWidth()
+                    .height(720.dp)
+            ) {
+                EndSection()
+            }
+        }
+    }
+}
+
+@Composable
+fun TextComponent(s: String) {
+    Text(s)
+}
+
+@Composable
+fun ImageComponent() {
+    Image(
+        painter = painterResource(id = R.drawable.img),
+        "Image",
+        modifier = Modifier.size(100.dp, 100.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PaviourTheme {
+        App()
+    }
+}
---
+++ app/src/main/java/com/github/paviour/composable will be here.kt
@@ -0,0 +1,235 @@
+package com.github.paviour
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.paviour.ui.theme.PaviourTheme
+
+@Composable
+fun TopSection(id: Int) {
+    Box() {
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            shape = RoundedCornerShape(12.dp),
+            elevation = 2.dp
+        ) {
+            Image(
+                painterResource(id),
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize()
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Column() {
+                    Text(
+                        text = "Constraint layout example with barriers",
+                        color = Color.White,
+                        fontSize = 20.sp
+                    )
+                    Text(
+                        text = "Constraint layout example",
+                        color = Color.White,
+                        fontSize = 10.sp
+                    )
+                    Column() {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = "H1", color = Color.White, fontSize = 40.sp)
+                            Text(text = "H2", color = Color.White, fontSize = 20.sp)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MiddleSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        ) {
+            ImageText(R.drawable.img, true)
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        ) {
+            ImageText(R.drawable.img)
+        }
+//        Column(modifier = Modifier.fillMaxWidth().weight(1.0f)) {
+//            TopSection(R.drawable.img)
+//        }
+//        Column(modifier = Modifier.fillMaxWidth().weight(1.0f)) {
+//            TopSection(R.drawable.img)
+//        }
+    }
+}
+
+@Composable
+fun EndSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        ) {
+            ImageText(R.drawable.img, true)
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        ) {
+            ImageText(R.drawable.img)
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        ) {
+            ImageText(R.drawable.img)
+        }
+//        Column(modifier = Modifier.fillMaxWidth().weight(1.0f)) {
+//            TopSection(R.drawable.img)
+//        }
+//        Column(modifier = Modifier.fillMaxWidth().weight(1.0f)) {
+//            TopSection(R.drawable.img)
+//        }
+    }
+}
+
+@Composable
+fun ImageCard(id: Int, flip: Boolean = false) {
+    Image(
+        painterResource(id),
+        contentDescription = "",
+        contentScale = ContentScale.FillBounds,
+        modifier = if (flip) {
+            Modifier
+                .fillMaxSize()
+                .scale(scaleX = -1f, scaleY = 1f)
+        } else {
+            Modifier.fillMaxSize()
+        }
+    )
+}
+
+@Composable
+fun ImageText(id: Int, flip: Boolean = false) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(12.dp),
+        elevation = 2.dp
+    ) {
+        CardElement(id, flip)
+    }
+}
+
+@Composable
+fun CardElement(id: Int, flip: Boolean) {
+    Row(
+        modifier = if (flip) {
+            Modifier
+                .scale(scaleX = -1f, scaleY = 1f)
+        } else {
+            Modifier.fillMaxWidth()
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        ) {
+            Text(
+                text = "Image View",
+                color = Color.Black,
+                fontSize = 50.sp
+            )
+            Text(
+                text = "Description",
+                color = Color.Black,
+                fontSize = 20.sp
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        ) {
+            Image(
+                painterResource(id),
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopSectionPreview() {
+    PaviourTheme {
+        TopSection(id = R.drawable.img)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MiddleSectionPreview() {
+    PaviourTheme {
+        MiddleSection()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EndSectionPreview() {
+    PaviourTheme {
+        EndSection()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ImageTextPreview() {
+    PaviourTheme {
+        ImageText(R.drawable.img)
+    }
+}
